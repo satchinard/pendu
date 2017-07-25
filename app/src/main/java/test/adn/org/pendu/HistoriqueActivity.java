59fd9fa41c0703f06c333001e573f2ac2697dc38
Ada ATCHINARD <satchinard@cagecfi.com>
2017-07-25 18:51:00 +0000

    Petite amélioration de la liste des meilleurs scores.

--- app/src/main/java/test/adn/org/pendu/HistoriqueActivity.java
+++ app/src/main/java/test/adn/org/pendu/HistoriqueActivity.java
@@ -20,14 +20,7 @@ public class HistoriqueActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historique);
-        listView = (ListView) findViewById(R.id.lv_scores);
-        listView.setAdapter(new JeuxScoreArrayAdapter(this, JeuxScoreArrayAdapter
-                .getScoreDatas(this)));
-        TextView textView = new TextView(getApplicationContext());
-        textView.setText(R.string.txt_meilleurs_scores);
-        textView.setTextColor(getResources().getColor(R.color.colorNiveau));
-        listView.addHeaderView(textView);
-        registerForContextMenu(listView);
+        populateListView();
     }
 
     @Override
@@ -42,11 +35,42 @@ public class HistoriqueActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.mn_tout_supprimer:
                 JeuxScoreArrayAdapter.deleteAll(getApplicationContext());
+                updateListView();
+//                listView.getAdapter().notify();
+//                final AlertDialog.Builder alertBuilder = new AlertDialog.Builder(getApplicationContext());
+//                alertBuilder.setIcon(R.drawable.ic_info_user);
+//                alertBuilder.setTitle(R.string.app_name);
+//                alertBuilder.setMessage(R.string.tst_historique_supprime);
+//                alertBuilder.setPositiveButton(getResources().getString(R.string.txt_ok), new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        dialog.dismiss();
+//                    }
+//                });
+//                alertBuilder.show();
                 Toast.makeText(this, R.string.tst_historique_supprime, Toast.LENGTH_SHORT).show();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+
+    private void populateListView() {
+        listView = (ListView) findViewById(R.id.lv_scores);
+        listView.setAdapter(new JeuxScoreArrayAdapter(this, JeuxScoreArrayAdapter
+                .getScoreDatas(this)));
+        TextView textView = new TextView(getApplicationContext());
+        textView.setText(R.string.txt_meilleurs_scores);
+        textView.setTextColor(getResources().getColor(R.color.colorNiveau));
+        listView.addHeaderView(textView);
+        registerForContextMenu(listView);
+    }
+
+    private void updateListView() {
+        listView = (ListView) findViewById(R.id.lv_scores);
+        listView.setAdapter(new JeuxScoreArrayAdapter(this, JeuxScoreArrayAdapter
+                .getScoreDatas(this)));
     }
 
 }
